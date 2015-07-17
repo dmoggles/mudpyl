6052fc0ca62d64a8870fd2ebd921e5daa195e613
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2015-07-16 20:06:12 -0500

    initial commit of existing mudpyl codebase

---
+++ mudpyl/gui/commands.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 16, 2015
+
+@author: Dmitry
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
